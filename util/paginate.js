2dda40d6fbd4ed66797d9f84fb669ef3fe2df6b9
Sohaib Rashed <sohaibrashed@gmail.com>
2024-11-06 13:53:59 +0500

    modified: improved paginate.js

--- util/paginate.js
+++ util/paginate.js
@@ -1,18 +1,30 @@
-// utils/pagination.js
+const paginate = async (model, queryParams) => {
+  const { page = 1, limit = process.env.LIMIT, ...filters } = queryParams;
 
-const paginate = (query, page, limit) => {
-  const currentPage = Math.max(1, parseInt(page) || 1);
-  const itemsPerPage = Math.max(1, parseInt(limit) || process.env.LIMIT);
-
+  const currentPage = Math.max(1, parseInt(page));
+  const itemsPerPage = Math.max(1, parseInt(limit));
   const skip = (currentPage - 1) * itemsPerPage;
 
-  query = query.skip(skip).limit(itemsPerPage);
+  // pipeline not working due to type casting
+  // const pipeline = [
+  //   { $match: filters },
+  //   { $skip: skip },
+  //   { $limit: itemsPerPage },
+  // ];
+
+  // const results = await model.aggregate(pipeline);
+
+  const results = await model.find(filters).skip(skip).limit(itemsPerPage);
+  const totalItems = await model.countDocuments(filters);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   return {
-    query,
+    data: results,
     pagination: {
       currentPage,
       itemsPerPage,
+      totalPages,
+      totalItems,
     },
   };
 };
